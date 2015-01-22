29d1c3b2e80395b2e0ac26ada0147984d0787e0e
cascadianblue <cboyle@macalester.edu>
2015-01-22 14:24:45 -0600

    Permission mixins

--- Utils/mixins.py
+++ Utils/mixins.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.decorators import permission_required as perm_required_decorator
 from django.utils.decorators import method_decorator
+from guardian.mixins import PermissionRequiredMixin
 
 
 class ModelPermRequiredMixin:
     permission_required = ''
 
-    @method_decorator(perm_required_decorator(permission_required))
+    @method_decorator(perm_required_decorator(permission_required, raise_exception=True))
     def dispatch(self, request):
         return super(ModelPermRequiredMixin, self).dispatch(request)
+
+
+class ObjPermRequiredMixin(PermissionRequiredMixin):
+    raise_exception = True
