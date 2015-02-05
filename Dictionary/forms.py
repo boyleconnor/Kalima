745b914a88fbd5d5b484e5a6deb9fc5ae40c5430
cascadianblue <cboyle@macalester.edu>
2015-02-05 16:49:07 -0600

    Abandoning user-friendly DeriverForm (i.e. contributors must write regex).

--- Dictionary/forms.py
+++ Dictionary/forms.py
@@ -1,4 +1,3 @@
-from ArabicTools.utils import form_to_template
 from Dictionary.models import Word, Deriver
 from django.forms import ModelForm
 from haystack.forms import SearchForm
@@ -14,14 +13,6 @@ class DeriverForm(ModelForm):
     class Meta:
         model = Deriver
         fields = '__all__'
-
-    def save(self, commit=True):
-        instance = super(DeriverForm, self).save(commit=False)
-        instance.origin_form = form_to_template(instance.origin_form)
-        instance.result_form = form_to_template(instance.result_form)
-        if commit:
-            instance.save()
-        return instance
 
 
 class WordSearchForm(SearchForm):
