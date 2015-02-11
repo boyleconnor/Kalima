881d5790540f488af493feae8d71bac7cf4c45c7
cascadianblue <cboyle@macalester.edu>
2015-02-11 17:37:17 -0600

    Added Util function that crudely converts origin_form's to something human-readable.

--- ArabicTools/utils.py
+++ ArabicTools/utils.py
@@ -7,16 +7,16 @@ def apply(origin_form, word, result_form):
     return re.match(origin_form, word).expand(result_form)
 
 
-def pattern_to_form(pattern):  # FIXME: This is fucking hideous
-    pattern_ = list(pattern)
+def pattern_to_form(pattern):  # FIXME: This will break for patterns expecting more than three letters
+    pattern = pattern.replace(LETTER, 'x')
+    form = ''
     default_root = list(DEFAULT_ROOT)
-    for i in range(len(pattern_)):
-        if (i+len(LETTER) <= len(pattern_)) and (''.join(pattern_[i:i+len(LETTER)]) == LETTER):
-            pattern_[i] = default_root.pop(0)
-            for j in range(i+1, i+len(LETTER)):
-                pattern_.pop(i+1)
-    pattern_ = ''.join(pattern_)
-    return pattern_
+    for letter in pattern:
+        if letter == 'x':
+            form += default_root.pop(0)
+        else:
+            form += letter
+    return form
 
 
 def transcribe(spelling, code):
