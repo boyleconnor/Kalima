3f17be6cb0f4a6476aaba07dfbeef69d0a77a3f8
cascadianblue <cboyle@macalester.edu>
2015-02-27 16:58:01 -0600

    Omitted 'word/' from search url

--- Dictionary/urls.py
+++ Dictionary/urls.py
@@ -1,11 +1,11 @@
-from Dictionary.forms import WordSearchForm
+from Dictionary.forms import SearchForm
 from django.conf.urls import patterns, include, url
 from Dictionary.views import WordCreate, WordDetail, WordUpdate, WordDelete, Home, WordSearch, DeriverDetail, \
     DeriverCreate, DeriverUpdate, DeriverApply
 from haystack.views import search_view_factory
 
 urlpatterns = patterns('',
-    url(r'word/search/$', WordSearch.as_view(), name='word.search'),
+    url(r'search/$', WordSearch.as_view(), name='word.search'),
     url(r'word/add/$', WordCreate.as_view(), name='word.create'),
     url(r'word/(?P<pk>\d+)/$', WordDetail.as_view(), name='word.detail'),
     url(r'word/(?P<pk>\d+)/edit/$', WordUpdate.as_view(), name='word.update'),
