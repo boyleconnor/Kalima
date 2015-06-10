b51f854d70711c207c09260f1f28b8785b4b05b3
cascadianblue <cboyle@macalester.edu>
2015-06-10 12:23:48 -0500

    Finished WordSerializer and DeriverSerializer

--- Dictionary/serializers.py
+++ Dictionary/serializers.py
@@ -5,8 +5,10 @@ from rest_framework.serializers import ModelSerializer
 class WordSerializer(ModelSerializer):
     class Meta:
         model = Word
+        fields = ('pos', 'spelling', 'definition', 'examples', 'stem', 'pattern')
 
 
 class DeriverSerializer(ModelSerializer):
     class Meta:
         model = Deriver
+        fields = ('origin_pos', 'result_pos', 'origin_form', 'result_form', 'origin_pattern', 'example_stem', 'name')
