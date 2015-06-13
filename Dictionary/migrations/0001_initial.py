f49e065462f4a8ef96645e23f6801216007b38c8
cascadianblue <cboyle@macalester.edu>
2015-06-13 18:47:05 -0500

    Migrations

--- Dictionary/migrations/0001_initial.py
+++ Dictionary/migrations/0001_initial.py
@@ -13,140 +13,34 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Deriver',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
-                ('origin_pos', models.CharField(choices=[('noun', 'Noun'), ('verb', 'Verb'), ('adjective', 'Adjective'), ('preposition', 'Preposition'), ('root', 'Root')], max_length=16)),
-                ('result_pos', models.CharField(choices=[('noun', 'Noun'), ('verb', 'Verb'), ('adjective', 'Adjective'), ('preposition', 'Preposition'), ('root', 'Root')], max_length=16)),
-                ('origin_form', models.CharField(default='([اويىبتثجحخدذرزسشصضطظعغفقكلمنهءة])([اويىبتثجحخدذرزسشصضطظعغفقكلمنهءة])([اويىبتثجحخدذرزسشصضطظعغفقكلمنهءة])', max_length=255)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('origin_pos', models.CharField(max_length=16, choices=[('noun', 'Noun'), ('verb', 'Verb'), ('adjective', 'Adjective'), ('preposition', 'Preposition'), ('root', 'Root')])),
+                ('result_pos', models.CharField(max_length=16, choices=[('noun', 'Noun'), ('verb', 'Verb'), ('adjective', 'Adjective'), ('preposition', 'Preposition'), ('root', 'Root')])),
+                ('origin_form', models.CharField(max_length=255, default='([اويىبتثجحخدذرزسشصضطظعغفقكلمنهءة])([اويىبتثجحخدذرزسشصضطظعغفقكلمنهءة])([اويىبتثجحخدذرزسشصضطظعغفقكلمنهءة])')),
                 ('result_form', models.CharField(max_length=255)),
-                ('example_stem', models.CharField(blank=True, max_length=4)),
-                ('name', models.CharField(blank=True, max_length=63)),
-                ('origin_pattern', models.ForeignKey(to='Dictionary.Deriver', null=True, related_name='result_patterns', blank=True)),
+                ('name', models.CharField(max_length=63, blank=True)),
             ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Inflecter',
-            fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
-                ('origin_form', models.CharField(max_length=255)),
-                ('result_form', models.CharField(max_length=255)),
-                ('case', models.CharField(choices=[('nominative', 'Nominative'), ('genitive', 'Genitive'), ('accusative', 'Accusative')], max_length=20, blank=True)),
-                ('state', models.CharField(choices=[('construct', 'Construct'), ('definite', 'Definite'), ('indefinite', 'Indefinite')], max_length=20, blank=True)),
-                ('number', models.CharField(choices=[('singular', 'Singular'), ('dual', 'Dual'), ('plural', 'Plural')], max_length=20, blank=True)),
-                ('gender', models.CharField(choices=[('masculine', 'Masculine'), ('feminine', 'Feminine')], max_length=20, blank=True)),
-                ('person', models.CharField(choices=[('first', 'First'), ('second', 'Second'), ('third', 'Third')], max_length=20, blank=True)),
-                ('tense', models.CharField(choices=[('perfect', 'Perfect'), ('indicative-imperfect', 'Indicative-Imperfect'), ('subjunctive-imperfect', 'Subjunctive-Imperfect'), ('jussive-imperfect', 'Jussive-Imperfect'), ('imperative', 'Imperative')], max_length=20, blank=True)),
-                ('voice', models.CharField(choices=[('active', 'Active'), ('passive', 'Passive')], max_length=20, blank=True)),
-                ('origin_pattern', models.ForeignKey(to='Dictionary.Deriver')),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Inflection',
-            fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
-                ('spelling', models.CharField(max_length=250)),
-                ('case', models.CharField(choices=[('nominative', 'Nominative'), ('genitive', 'Genitive'), ('accusative', 'Accusative')], max_length=20, blank=True)),
-                ('state', models.CharField(choices=[('construct', 'Construct'), ('definite', 'Definite'), ('indefinite', 'Indefinite')], max_length=20, blank=True)),
-                ('number', models.CharField(choices=[('singular', 'Singular'), ('dual', 'Dual'), ('plural', 'Plural')], max_length=20, blank=True)),
-                ('gender', models.CharField(choices=[('masculine', 'Masculine'), ('feminine', 'Feminine')], max_length=20, blank=True)),
-                ('person', models.CharField(choices=[('first', 'First'), ('second', 'Second'), ('third', 'Third')], max_length=20, blank=True)),
-                ('tense', models.CharField(choices=[('perfect', 'Perfect'), ('indicative-imperfect', 'Indicative-Imperfect'), ('subjunctive-imperfect', 'Subjunctive-Imperfect'), ('jussive-imperfect', 'Jussive-Imperfect'), ('imperative', 'Imperative')], max_length=20, blank=True)),
-                ('voice', models.CharField(choices=[('active', 'Active'), ('passive', 'Passive')], max_length=20, blank=True)),
-                ('pattern', models.ForeignKey(to='Dictionary.Inflecter', null=True, blank=True)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Word',
             fields=[
-                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
-                ('pos', models.CharField(choices=[('noun', 'Noun'), ('verb', 'Verb'), ('adjective', 'Adjective'), ('preposition', 'Preposition'), ('root', 'Root')], max_length=20)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('pos', models.CharField(max_length=20, choices=[('noun', 'Noun'), ('verb', 'Verb'), ('adjective', 'Adjective'), ('preposition', 'Preposition'), ('root', 'Root')])),
                 ('spelling', models.CharField(max_length=255)),
                 ('definition', models.TextField(verbose_name='Definition')),
                 ('examples', models.TextField(verbose_name='Examples', blank=True)),
+                ('pattern', models.ForeignKey(to='Dictionary.Deriver', blank=True, null=True, related_name='words')),
+                ('stem', models.ForeignKey(to='Dictionary.Word', blank=True, null=True, related_name='derivatives')),
             ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='Verb',
-            fields=[
-                ('word_ptr', models.OneToOneField(serialize=False, auto_created=True, to='Dictionary.Word', primary_key=True, parent_link=True)),
-            ],
-            options={
-            },
-            bases=('Dictionary.word',),
-        ),
-        migrations.CreateModel(
-            name='Root',
-            fields=[
-                ('word_ptr', models.OneToOneField(serialize=False, auto_created=True, to='Dictionary.Word', primary_key=True, parent_link=True)),
-                ('length', models.SmallIntegerField(choices=[(3, 'Three'), (4, 'Four')])),
-            ],
-            options={
-            },
-            bases=('Dictionary.word',),
-        ),
-        migrations.CreateModel(
-            name='Noun',
-            fields=[
-                ('word_ptr', models.OneToOneField(serialize=False, auto_created=True, to='Dictionary.Word', primary_key=True, parent_link=True)),
-                ('gender', models.CharField(choices=[('masculine', 'Masculine'), ('feminine', 'Feminine')], max_length=20)),
-                ('human', models.BooleanField(default=False)),
-            ],
-            options={
-            },
-            bases=('Dictionary.word',),
-        ),
-        migrations.CreateModel(
-            name='Adverb',
-            fields=[
-                ('word_ptr', models.OneToOneField(serialize=False, auto_created=True, to='Dictionary.Word', primary_key=True, parent_link=True)),
-            ],
-            options={
-            },
-            bases=('Dictionary.word',),
-        ),
-        migrations.CreateModel(
-            name='Adjective',
-            fields=[
-                ('word_ptr', models.OneToOneField(serialize=False, auto_created=True, to='Dictionary.Word', primary_key=True, parent_link=True)),
-                ('noun', models.ManyToManyField(to='Dictionary.Noun')),
-            ],
-            options={
-            },
-            bases=('Dictionary.word',),
         ),
         migrations.AddField(
-            model_name='word',
-            name='pattern',
-            field=models.ForeignKey(to='Dictionary.Deriver', null=True, related_name='words', blank=True),
-            preserve_default=True,
+            model_name='deriver',
+            name='example_stem',
+            field=models.ForeignKey(to='Dictionary.Word', blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='word',
-            name='stem',
-            field=models.ForeignKey(to='Dictionary.Word', null=True, related_name='derivatives', blank=True),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='verb',
-            name='noun',
-            field=models.ManyToManyField(to='Dictionary.Noun'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='inflection',
-            name='stem',
-            field=models.ForeignKey(to='Dictionary.Word'),
-            preserve_default=True,
+            model_name='deriver',
+            name='origin_pattern',
+            field=models.ForeignKey(to='Dictionary.Deriver', blank=True, null=True, related_name='result_patterns'),
         ),
     ]
--- Inflections/migrations/0001_initial.py
+++ Inflections/migrations/0001_initial.py
@@ -7,40 +7,40 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('Dictionary', '0003_auto_20150612_1940'),
+        ('Dictionary', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Inflecter',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('origin_form', models.CharField(max_length=255)),
                 ('result_form', models.CharField(max_length=255)),
-                ('case', models.CharField(choices=[('nominative', 'Nominative'), ('genitive', 'Genitive'), ('accusative', 'Accusative')], blank=True, max_length=20)),
-                ('state', models.CharField(choices=[('construct', 'Construct'), ('definite', 'Definite'), ('indefinite', 'Indefinite')], blank=True, max_length=20)),
-                ('number', models.CharField(choices=[('singular', 'Singular'), ('dual', 'Dual'), ('plural', 'Plural')], blank=True, max_length=20)),
-                ('gender', models.CharField(choices=[('masculine', 'Masculine'), ('feminine', 'Feminine')], blank=True, max_length=20)),
-                ('person', models.CharField(choices=[('first', 'First'), ('second', 'Second'), ('third', 'Third')], blank=True, max_length=20)),
-                ('tense', models.CharField(choices=[('perfect', 'Perfect'), ('indicative-imperfect', 'Indicative-Imperfect'), ('subjunctive-imperfect', 'Subjunctive-Imperfect'), ('jussive-imperfect', 'Jussive-Imperfect'), ('imperative', 'Imperative')], blank=True, max_length=20)),
-                ('voice', models.CharField(choices=[('active', 'Active'), ('passive', 'Passive')], blank=True, max_length=20)),
+                ('case', models.CharField(max_length=20, blank=True, choices=[('nominative', 'Nominative'), ('genitive', 'Genitive'), ('accusative', 'Accusative')])),
+                ('state', models.CharField(max_length=20, blank=True, choices=[('construct', 'Construct'), ('definite', 'Definite'), ('indefinite', 'Indefinite')])),
+                ('number', models.CharField(max_length=20, blank=True, choices=[('singular', 'Singular'), ('dual', 'Dual'), ('plural', 'Plural')])),
+                ('gender', models.CharField(max_length=20, blank=True, choices=[('masculine', 'Masculine'), ('feminine', 'Feminine')])),
+                ('person', models.CharField(max_length=20, blank=True, choices=[('first', 'First'), ('second', 'Second'), ('third', 'Third')])),
+                ('tense', models.CharField(max_length=20, blank=True, choices=[('perfect', 'Perfect'), ('indicative-imperfect', 'Indicative-Imperfect'), ('subjunctive-imperfect', 'Subjunctive-Imperfect'), ('jussive-imperfect', 'Jussive-Imperfect'), ('imperative', 'Imperative')])),
+                ('voice', models.CharField(max_length=20, blank=True, choices=[('active', 'Active'), ('passive', 'Passive')])),
                 ('origin_pattern', models.ForeignKey(to='Dictionary.Deriver')),
             ],
         ),
         migrations.CreateModel(
             name='Inflection',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
                 ('spelling', models.CharField(max_length=250)),
                 ('attributes', models.TextField()),
-                ('case', models.CharField(choices=[('nominative', 'Nominative'), ('genitive', 'Genitive'), ('accusative', 'Accusative')], blank=True, max_length=20)),
-                ('state', models.CharField(choices=[('construct', 'Construct'), ('definite', 'Definite'), ('indefinite', 'Indefinite')], blank=True, max_length=20)),
-                ('number', models.CharField(choices=[('singular', 'Singular'), ('dual', 'Dual'), ('plural', 'Plural')], blank=True, max_length=20)),
-                ('gender', models.CharField(choices=[('masculine', 'Masculine'), ('feminine', 'Feminine')], blank=True, max_length=20)),
-                ('person', models.CharField(choices=[('first', 'First'), ('second', 'Second'), ('third', 'Third')], blank=True, max_length=20)),
-                ('tense', models.CharField(choices=[('perfect', 'Perfect'), ('indicative-imperfect', 'Indicative-Imperfect'), ('subjunctive-imperfect', 'Subjunctive-Imperfect'), ('jussive-imperfect', 'Jussive-Imperfect'), ('imperative', 'Imperative')], blank=True, max_length=20)),
-                ('voice', models.CharField(choices=[('active', 'Active'), ('passive', 'Passive')], blank=True, max_length=20)),
-                ('pattern', models.ForeignKey(blank=True, to='Inflections.Inflecter', null=True)),
+                ('case', models.CharField(max_length=20, blank=True, choices=[('nominative', 'Nominative'), ('genitive', 'Genitive'), ('accusative', 'Accusative')])),
+                ('state', models.CharField(max_length=20, blank=True, choices=[('construct', 'Construct'), ('definite', 'Definite'), ('indefinite', 'Indefinite')])),
+                ('number', models.CharField(max_length=20, blank=True, choices=[('singular', 'Singular'), ('dual', 'Dual'), ('plural', 'Plural')])),
+                ('gender', models.CharField(max_length=20, blank=True, choices=[('masculine', 'Masculine'), ('feminine', 'Feminine')])),
+                ('person', models.CharField(max_length=20, blank=True, choices=[('first', 'First'), ('second', 'Second'), ('third', 'Third')])),
+                ('tense', models.CharField(max_length=20, blank=True, choices=[('perfect', 'Perfect'), ('indicative-imperfect', 'Indicative-Imperfect'), ('subjunctive-imperfect', 'Subjunctive-Imperfect'), ('jussive-imperfect', 'Jussive-Imperfect'), ('imperative', 'Imperative')])),
+                ('voice', models.CharField(max_length=20, blank=True, choices=[('active', 'Active'), ('passive', 'Passive')])),
+                ('pattern', models.ForeignKey(to='Inflections.Inflecter', blank=True, null=True)),
                 ('stem', models.ForeignKey(to='Dictionary.Word')),
             ],
         ),
