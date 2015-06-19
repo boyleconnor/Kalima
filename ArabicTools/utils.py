6eb450066d6ebcd080b2b7bebddf65c888feabbb
cascadianblue <cboyle@macalester.edu>
2015-06-19 15:12:04 -0500

    Added functions pertaining to attributes and inflection attributes to ArabicTools.utils

--- ArabicTools/utils.py
+++ ArabicTools/utils.py
@@ -1,6 +1,8 @@
-from ArabicTools.constants import DIACRITICS, SHADDA, DEFAULT_ROOT_SPELLING
+from ArabicTools.constants import DIACRITICS, SHADDA, DEFAULT_ROOT_SPELLING, ALLOWED_INFLECTION_ATTRIBUTES, POS_CHOICES, \
+    ALLOWED_INFLECTION_ATTRIBUTE_VALUES
 import re
 from ArabicTools.regex import LETTER
+from django.core.exceptions import ValidationError
 
 
 def apply(origin_form, word, result_form):
@@ -47,3 +49,38 @@ def strip_diacritics(spelling, leave=[]):
             output += letter
     return output
 
+
+def attributes_to_dict(attributes):
+    '''Converts a string containing attributes to a Python dictionary without performing any validation
+    '''
+    lines = attributes.split('\n')
+    dict_out = {}
+    for line in lines:
+        attribute, value = line.split(':')
+        dict_out[attribute] = value
+    return dict_out
+
+
+def dict_to_attributes(dict_in):
+    ''' Converts a Python dictionary to a string containing attributes
+    '''
+    attributes = []
+    for attribute, value in dict_in:
+        attributes += [attribute + ':' + value + '\n']
+    attributes = attributes.join('\n')
+    return attributes
+
+
+def validate_inflection_attributes(part_of_speech, attributes):
+    if part_of_speech not in dict(POS_CHOICES):
+        raise ValidationError('part_of_speech not allowed part of speech')
+    if part_of_speech not in ALLOWED_INFLECTION_ATTRIBUTES:
+        raise ValidationError('part_of_speech does not have inflection attributes')
+    attribute_dict = attributes_to_dict(attributes)
+    for attribute in attribute_dict:
+        if attribute not in ALLOWED_INFLECTION_ATTRIBUTE_VALUES:
+            raise ValidationError('%s is not an allowed attribute' % attribute)
+        if attribute not in ALLOWED_INFLECTION_ATTRIBUTES[part_of_speech]:
+            raise ValidationError('%s is not an allowed attribute for %s' % (attribute, part_of_speech))
+        if attribute_dict[attribute] not in ALLOWED_INFLECTION_ATTRIBUTE_VALUES[attribute]:
+            raise ValidationError('%s is not a valid value for attribute %s' % (attribute_dict[attribute], attribute))
