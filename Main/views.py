8c917add766deea33379042f69cdef7d46bf1fdd
cascadianblue <cboyle@macalester.edu>
2015-07-11 14:53:15 -0500

    Main.views.HomeView now passes a randomly-ordered queryset of words through the template context under 'words'

--- Main/views.py
+++ Main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from Dictionary.models import Word
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['words'] = Word.objects.order_by('?')
+        return context
