643bf87b458d4b7fc6edb3640bba9df7990e0280
cascadianblue <cboyle@macalester.edu>
2015-07-12 19:29:09 -0500

    Added url for Dictionary.views.WordDetailHTML

--- Dictionary/urls.py
+++ Dictionary/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns('',
     url(r'^word/$', views.WordList.as_view(), name='word.list'),
     url(r'^word/(?P<pk>\d+)/$', views.WordDetail.as_view(), name='word.detail'),
+    url(r'^word/(?P<pk>\d+)/html/$', views.WordDetailHTML.as_view(), name='word.detail.html'),
     url(r'^pattern/$', views.PatternList.as_view(), name='pattern.list'),
     url(r'^pattern/(?P<pk>\d+)/$', views.PatternDetail.as_view(), name='pattern.detail'),
     url(r'^apply/$', views.PatternApply.as_view(), name='apply')
