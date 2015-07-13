3a4b2af5ee196f841ab65e528dd55242d18916d1
cascadianblue <cboyle@macalester.edu>
2015-07-12 19:26:07 -0500

    Added WordDetailHTML view.

--- Dictionary/views.py
+++ Dictionary/views.py
@@ -1,6 +1,7 @@
 from Dictionary.models import Word, Pattern
 from Dictionary.serializers import WordSerializer, PatternSerializer, ApplySerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
+from django.views.generic import DetailView
 
 
 class WordList(ListCreateAPIView):
@@ -11,6 +12,11 @@ class WordList(ListCreateAPIView):
 class WordDetail(RetrieveUpdateDestroyAPIView):
     queryset = Word.objects.all()
     serializer_class = WordSerializer
+
+
+class WordDetailHTML(DetailView):
+    model = Word
+    template_name = 'word/detail.html'
 
 
 class PatternList(ListCreateAPIView):
