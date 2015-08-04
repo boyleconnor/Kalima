3d5011fda17a16c8b13e4e722ad675541411793c
cascadianblue <cboyle@macalester.edu>
2015-08-03 23:08:30 -0500

    Switched Dictionary views to API-only, deleted Dictionary templates.

--- Dictionary/urls.py
+++ Dictionary/urls.py
@@ -2,11 +2,9 @@ from Dictionary import views
 from django.conf.urls import url, patterns
 
 urlpatterns = patterns('',
-    url(r'^word/$', views.WordListJSON.as_view(), name='word.list'),
-    url(r'^word/(?P<pk>\d+)/$', views.WordDetailJSON.as_view(), name='word.detail'),
-    url(r'^word/html/$', views.WordListHTML.as_view(), name='word.list.html'),
-    url(r'^word/(?P<pk>\d+)/html/$', views.WordDetailHTML.as_view(), name='word.detail.html'),
-    url(r'^pattern/$', views.PatternListJSON.as_view(), name='pattern.list'),
-    url(r'^pattern/(?P<pk>\d+)/$', views.PatternDetailJSON.as_view(), name='pattern.detail'),
-    url(r'^apply/$', views.PatternApplyJSON.as_view(), name='apply')
+    url(r'^word/$', views.WordList.as_view(), name='word.list'),
+    url(r'^word/(?P<pk>\d+)/$', views.WordDetail.as_view(), name='word.detail'),
+    url(r'^pattern/$', views.PatternList.as_view(), name='pattern.list'),
+    url(r'^pattern/(?P<pk>\d+)/$', views.PatternDetail.as_view(), name='pattern.detail'),
+    url(r'^apply/$', views.PatternApply.as_view(), name='apply')
 )
--- Dictionary/views.py
+++ Dictionary/views.py
@@ -4,43 +4,25 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from django.views.generic import DetailView, ListView
 
 
-class WordListJSON(ListCreateAPIView):
+class WordList(ListCreateAPIView):
     queryset = Word.objects.all()
     serializer_class = WordSerializer
 
 
-class WordDetailJSON(RetrieveUpdateDestroyAPIView):
+class WordDetail(RetrieveUpdateDestroyAPIView):
     queryset = Word.objects.all()
     serializer_class = WordSerializer
 
 
-class WordDetailHTML(DetailView):
-    model = Word
-    template_name = 'word/detail.html'
-
-
-class WordListHTML(ListView):
-    model = Word
-    template_name = 'word/list.html'
-
-    def filter_queryset(self, queryset):
-        if self.request.GET:
-            return queryset.filter(**self.request.GET.dict())
-        return queryset
-
-    def get_queryset(self):
-        queryset = super(WordListHTML, self).get_queryset()
-        return self.filter_queryset(queryset)
-
-class PatternListJSON(ListCreateAPIView):
+class PatternList(ListCreateAPIView):
     queryset = Pattern.objects.all()
     serializer_class = PatternSerializer
 
 
-class PatternDetailJSON(RetrieveUpdateDestroyAPIView):
+class PatternDetail(RetrieveUpdateDestroyAPIView):
     queryset = Pattern.objects.all()
     serializer_class = PatternSerializer
 
 
-class PatternApplyJSON(CreateAPIView):
+class PatternApply(CreateAPIView):
     serializer_class = ApplySerializer
