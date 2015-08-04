bb2c33a15bec8e26a6429ff92b43c81c4e2b4122
cascadianblue <cboyle@macalester.edu>
2015-08-03 23:32:26 -0500

    Added actual url pointing to Word view in Main

--- Main/urls.py
+++ Main/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
-from Main.views import HomeView
+from Main.views import HomeView, WordView
 
 
 urlpatterns = patterns('',
     url('^$', HomeView.as_view(), name='home'),
+    url('^word/(?P<pk>\d+)/$', WordView.as_view(), name='word'),
 )
