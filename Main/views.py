a2c11a91792afc91b8fa17b947898b8017d0a9ff
cascadianblue <cboyle@macalester.edu>
2015-08-03 22:52:39 -0500

    Added Word view to Main and corresponding template

--- Main/views.py
+++ Main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from Dictionary.models import Word
+
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -9,3 +10,8 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['words'] = Word.objects.order_by('?')
         return context
+
+
+class WordView(DetailView):
+    template_name = 'word.html'
+    model = Word
