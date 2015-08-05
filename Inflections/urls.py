4364935d6c39020cdfa5d7629db826b827d5cee1
cascadianblue <cboyle@macalester.edu>
2015-08-05 00:40:45 -0500

    Added StemList and StemDetail API views and corresponding URLs

--- Inflections/urls.py
+++ Inflections/urls.py
@@ -4,4 +4,6 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns('',
     url(r'^inflection/$', views.InflectionList.as_view(), name='inflection.list'),
     url(r'^inflection/(?P<pk>\d+)/$', views.InflectionDetail.as_view(), name='inflection.detail'),
+    url(r'^stem/$', views.StemList.as_view(), name='stem.list'),
+    url(r'^stem/(?P<pk>\d+)/$', views.StemDetail.as_view(), name='stem.detail'),
 )
--- Inflections/views.py
+++ Inflections/views.py
@@ -1,5 +1,5 @@
-from Inflections.models import Inflection
-from Inflections.serializers import InflectionSerializer
+from Inflections.models import Inflection, Stem
+from Inflections.serializers import InflectionSerializer, StemSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 
@@ -11,3 +11,13 @@ class InflectionList(ListCreateAPIView):
 class InflectionDetail(RetrieveUpdateDestroyAPIView):
     queryset = Inflection.objects.all()
     serializer_class = InflectionSerializer
+
+
+class StemList(ListCreateAPIView):
+    queryset = Stem.objects.all()
+    serializer_class = StemSerializer
+
+
+class StemDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Stem.objects.all()
+    serializer_class = StemSerializer
