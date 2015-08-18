d862c36e21b6ea6647498cb14c257b138c250109
cascadianblue <cboyle@macalester.edu>
2015-08-17 20:05:34 -0500

    Added id fields to all ModelSerializers in Serializers

--- Inflections/serializers.py
+++ Inflections/serializers.py
@@ -5,28 +5,28 @@ from rest_framework import serializers
 class InflectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Inflection
-        fields = ('spelling', 'stem', 'pattern', 'attributes')
+        fields = ('id', 'spelling', 'stem', 'pattern', 'attributes')
 
 
 class StemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stem
-        fields = ('spelling', 'parent', 'parent', 'exemplar', 'attributes')
+        fields = ('id', 'spelling', 'parent', 'parent', 'exemplar', 'attributes')
 
 
 class StemmerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stemmer
-        fields = ('origin_form', 'get_origin_form', 'result_form', 'get_result_form', 'origin_pattern')
+        fields = ('id', 'origin_form', 'get_origin_form', 'result_form', 'get_result_form', 'origin_pattern')
 
 
 class ParadigmSerializer(serializers.ModelSerializer):
     class Meta:
         model = Paradigm
-        fields = ('name',)
+        fields = ('id', 'name',)
 
 
 class InflecterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Inflecter
-        fields = ('paradigm', 'attributes', 'prefix', 'suffix')
+        fields = ('id', 'paradigm', 'attributes', 'prefix', 'suffix')
