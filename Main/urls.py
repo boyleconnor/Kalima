93fbd6db16b0d10278f0c65c5cebe7a88795fa48
cascadianblue <connor.bo@gmail.com>
2016-03-05 10:45:36 +0800

    Add Url for AddWordView
    Adds url for AddWordView at word/add/ in Main/urls.py

--- Main/urls.py
+++ Main/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
-from Main.views import HomeView, WordView, PatternView
+from Main.views import HomeView, WordView, AddWordView, PatternView
 
 
 urlpatterns = patterns('',
     url('^$', HomeView.as_view(), name='home'),
     url('^word/(?P<pk>\d+)/$', WordView.as_view(), name='word'),
+    url('^word/add/$', AddWordView.as_view(), name='add_word'),
     url('^pattern/(?P<pk>\d+)/$', PatternView.as_view(), name='pattern'),
 )
