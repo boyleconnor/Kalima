873cb22062b195a0f933b93ccc4b37852c717bc3
cascadianblue <connor.bo@gmail.com>
2016-12-01 00:41:32 -0600

    Add Tests to Inflections
    Adds tests for the "apply" functionality of the Stemmer and Inflecter
    models.

--- Inflections/tests.py
+++ Inflections/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from ArabicTools.constants import FAA, EIN, LAM, FATHA, DAMMA, SUKUN, KAF, TAA, BAA, YAA
+from ArabicTools.regex import SOUND_LETTER
+from ArabicTools.models import Special, SpecialSet
+from Dictionary.models import Word
+from Inflections.models import Stemmer, Stem, Inflecter
 
-# Create your tests here.
+
+class StemmerTestCase(TestCase):
+    def test_stemmer_apply(self):
+        special_set = SpecialSet.objects.create(name='Sound Triliteral')
+        for special_letter in (FAA, EIN, LAM):
+            special_set.specials.create(key=special_letter, value=SOUND_LETTER)
+        word = Word.objects.create(spelling=KAF + FATHA + TAA + FATHA + BAA + FATHA)
+
+        origin_form = FAA + FATHA + EIN + FATHA + LAM + FATHA
+        result_form = FATHA + FAA + SUKUN + EIN + DAMMA + LAM
+        stemmer = Stemmer.objects.create(
+            attributes='tense:present\n'+'mood:indicative',
+            origin_form=origin_form,
+            result_form=result_form,
+            special_set=special_set
+        )
+        result = stemmer.apply(origin=word)
+        self.assertEqual(result.spelling, FATHA+KAF+SUKUN+TAA+DAMMA+BAA)
+
+
+class InflecterTestCase(TestCase):
+    def test_inflecter_apply(self):
+        special_set = SpecialSet.objects.create(name='Sound Triliteral')
+        for special_letter in (FAA, EIN, LAM):
+            special_set.specials.create(key=special_letter, value=SOUND_LETTER)
+        word = Word.objects.create(spelling=KAF + FATHA + TAA + FATHA + BAA + FATHA) # kataba
+
+        stem = Stem.objects.create(spelling=FATHA + KAF + SUKUN + TAA + DAMMA + BAA, parent=word)
+        inflecter = Inflecter.objects.create(prefix=YAA, suffix=DAMMA)
+        result = inflecter.apply(stem=stem)
+        self.assertEqual(result.spelling, YAA+FATHA+KAF+SUKUN+TAA+DAMMA+BAA+DAMMA)
